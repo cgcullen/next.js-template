c54289edc3af323c08d1c7b3c42db6e41d98031b
Cindy Cullen <cindycullen@gmail.com>
2020-05-30 12:04:38 -0500

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import Nav from '../components/nav'
+import Nav from "@/components/Nav"
 
 export default function IndexPage() {
   return (
     <div>
       <Nav />
       <div className="hero">
-        <h1 className="title">Next.js + Tailwind CSS</h1>
+        <h1 className="title">Cindy's Template</h1>
       </div>
     </div>
   )
